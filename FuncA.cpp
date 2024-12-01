a7a37c649005863ad378acb37d5185bff90886e1
Rost <rostics@ubuntu19.myguest.virtualbox.org>
2024-12-01 16:09:49 +0200

    FuncA implementation for first 3 elements

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,6 +1,15 @@
 #include "FuncA.h"
+#include <cmath>
 
-double FuncA::calculate() {
-    return 0; // Початкова реалізація
+// Реалізація обчислення для перших 3 елементів
+double FuncA::calculate() const {
+    int n = 3;
+    double result = 0.0;
+    for (int k = 1; k <= n; ++k) {
+        double term = std::pow(4.0, k) * (std::pow(4.0, k) - 1) / (std::tgamma(2 * k + 1));
+        result += term;
+    }
+    return result;
 }
+
 
