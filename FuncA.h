377e8909ae6454db7e27807a4aef3daef1248b4a
Rost <rostics@ubuntu19.myguest.virtualbox.org>
2024-12-01 15:54:08 +0200

    FuncA returning 0

---
+++ FuncA.h
@@ -0,0 +1,10 @@
+#ifndef FUNCA_H
+#define FUNCA_H
+
+class FuncA {
+public:
+    FuncA() {}
+    double calculate(); // Повертає значення 0
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "FuncA.h"
+
+int main() {
+    FuncA func;
+    std::cout << "FuncA result: " << func.calculate() << std::endl;
+    return 0;
+}
+
